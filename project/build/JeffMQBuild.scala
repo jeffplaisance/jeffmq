a429085f0bdbed434134bf65acf80001b27b2bac
Jeff Plaisance <jeffplaisance@gmail.com>
2010-08-17 10:50:50 -0500

    moving everything around to comply with standard practice
    Signed-off-by: Jeff Plaisance <jeffplaisance@gmail.com>

---
+++ project/build/JeffMQBuild.scala
@@ -0,0 +1,12 @@
+import sbt._
+
+class ScalaProtobuf(info: ProjectInfo) extends DefaultProject(info) {
+    override def pomExtra =
+        <licenses>
+            <license>
+                <name>Apache 2</name>
+                <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
+                <distribution>repo</distribution>
+            </license>
+        </licenses>
+}
--- src/main/scala/com/jeffplaisance/jeffmq/JeffMQConsole.scala
+++ src/main/scala/com/jeffplaisance/jeffmq/JeffMQConsole.scala
@@ -9,7 +9,7 @@
 // distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and limitations under the License.
 
-package jeffmq
+package com.jeffplaisance.jeffmq
 
 import java.util.concurrent.LinkedBlockingQueue
 import org.jboss.netty.buffer.ChannelBuffers
